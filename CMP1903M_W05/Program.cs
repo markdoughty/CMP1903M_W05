998d6ad65071a26f83d8a134d751d29949b50be8
Mark Doughty <mdoughty@lincoln.ac.uk>
2020-02-14 17:13:07 +0000

    Add project files.

---
+++ CMP1903M_W05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMP1903M_W05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
