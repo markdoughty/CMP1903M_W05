252de2c3dff5f24ec879a723f25a00ad8a75d3b9
Josh <alphajts@gmail.com>
2020-02-18 14:01:33 +0000

    NEW

--- CMP1903M_W05/Program.cs
+++ CMP1903M_W05/Program.cs
@@ -6,7 +6,16 @@ namespace CMP1903M_W05
     {
         static void Main(string[] args)
         {
-            Student s = new Student();
+            Student s = new Student("Josh",19,1);
+            Student s2 = new Student("Joshua", 20, 1);
+            s.Status();
+             University U = new University("Lincoln", 1);
+            U.addStudent(s);
+            U.addStudent(s2);
+
+            U.getNumberOfStudents();
+
+            U.getAgeRangeOfStudents();
         }
     }
 }
--- CMP1903M_W05/Student.cs
+++ CMP1903M_W05/Student.cs
@@ -14,5 +14,35 @@ namespace CMP1903M_W05
         public string UniSchool { get; set; }
         public int courseID { get; set; }
         public string UniCourse { get; set; }
+        public int Year { get; set; }
+
+        public Student()
+        {
+            //Console.WriteLine("This is the default");
+            Name = "defaultName";
+            Age = 0;
+            studentID = 0;
+            UniCollege = "defaultCollage";
+            UniSchool = "defaultSchool";
+            courseID = 0;
+            UniCourse = "defaultCourse";
+            Year = 1;
+        }
+
+        public Student(string name, int age, int Y): this()
+        {
+            Name = name;
+            Age = age;
+            Year = Y;
+            //Console.WriteLine($"Student - Name: {Name}, Age: {Age}, Year of Study: {Year}");
+        }
+
+        public void Status()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"student ID: {studentID}");
+            Console.WriteLine($"Year of study: {Year}");
+        }
     }
 }
